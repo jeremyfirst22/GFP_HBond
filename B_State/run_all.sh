a10e077b0549c28b67114b4988098c084120b863
jeremyfirst22 <jeremy_first@utexas.edu>
2017-04-21 11:15:54 -0500

    Added three nitrile locations

--- B_State/run_all.sh
+++ B_State/run_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-molecList="GFP_Y92X GFP_F114X GFP_Y143X GFP_F145X GFP_Y149X GFP_F165X GFP_N212X GFP_M218X" 
+#molecList="GFP_Y92X GFP_F114X GFP_Y143X GFP_F145X GFP_Y149X GFP_F165X GFP_N212X GFP_M218X" 
+molecList="GFP_D117X GFP_D190X GFP_N198X" 
 
 for molec in $molecList ; do 
     if [ ! -f StartingStructures/$molec.pdb ] ; then 
