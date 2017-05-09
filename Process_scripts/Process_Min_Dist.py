8988a4f8545d3cdf17a887400cdd0f7512296ac6
jeremyfirst22 <jeremy_first@utexas.edu>
2017-05-09 10:04:32 -0500

    Flexible number of data points

--- Process_scripts/Process_Min_Dist.py
+++ Process_scripts/Process_Min_Dist.py
@@ -5,13 +5,16 @@ import os
 from os import sys
 from matplotlib.colors import LogNorm
 
+figCols=4
+figRows=3
+
 if not os.path.isdir('figures') : 
     os.mkdir('figures') 
 
 for state in ['A','B'] : 
     datafiles = glob.glob('%s_State/*/min_dist/cnf_water.xvg'%state) 
     
-    fig, axarr = plt.subplots(2,4,sharex='col',sharey='row') 
+    fig, axarr = plt.subplots(figRows,figCols,sharex='col',sharey='row') 
     fig.subplots_adjust(wspace=0) 
     fig.text(0.5,0.04, "Time (ns)", ha='center', va='center') 
     fig.text(0.08,0.5, r"Distance to nearest water ($\AA$)", ha='center', va='center',rotation='vertical') 
@@ -21,7 +24,7 @@ for state in ['A','B'] :
         data[:,0] = data[:,0] / 1000 * 4
         data[:,1] = data[:,1] * 10 # nm -> Angstroms
     
-        ax = axarr[index/4,index%4]
+        ax = axarr[index/figCols,index%figCols]
     
         ax.scatter(data[:,0],data[:,1],s=0.1) 
         ax.set_title(file.split('/')[1]) 
