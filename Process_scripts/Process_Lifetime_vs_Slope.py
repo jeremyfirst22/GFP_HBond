8ab59d64f360f5482aa51a207a98c4e3a019aea0
jeremyfirst22 <jeremy_first@utexas.edu>
2018-02-09 12:03:24 -0600

    Lifetime decays are fitted to exponential functions. Fitting done in Mathematica and parameters stored in seperate file

--- Process_scripts/Process_Lifetime_vs_Slope.py
+++ Process_scripts/Process_Lifetime_vs_Slope.py
@@ -1,69 +1,116 @@
+import glob 
 import numpy as np 
 import matplotlib.pyplot as plt 
-import glob as glob 
-import os 
-from os import sys
-from scipy.stats import linregress
-from matplotlib import rc_file
+from matplotlib.colors import LogNorm
+import os
+from sys import exit
 
-inFile='Exp_data/sasa_abs_data.dat'  
-rcFile='paper.rc'
+figCols=3
+figRows=3
 
-saveDir='figures'
+if not os.path.isdir('figures') : 
+    os.mkdir('figures') 
 
-if not os.path.isdir(saveDir) : 
-    os.mkdir(saveDir)
+datafiles = glob.glob('B_State/*/hbond_2/persistent.xvg') 
 
-## Plot for both states
-for state in ['A','B'] : 
-    rc_file("rc_files/%s"%rcFile) 
-    fig, ax = plt.subplots(1,1) 
+fig, axarr = plt.subplots(figRows,figCols,sharex='col',sharey='row') 
+fig.subplots_adjust(wspace=0,hspace=0.30) 
+fig.text(0.5,0.04, "Time (ns)", ha='center', va='center') 
+#fig.text(0.5,0.04, "Frames", ha='center', va='center') 
 
-    names = np.genfromtxt(inFile,skip_header=1,usecols=0,dtype='str')
-    slopes= np.genfromtxt(inFile,skip_header=1,usecols=1) 
-    sasas = np.zeros(len(slopes))
-    stds = np.zeros(np.size(sasas) ) 
+with open('fits/water_chosen_decay.xvg') as f: 
+    lines = f.readlines() 
+    fitKeys= {} 
+    for line in lines : 
+        splitline = line.split() 
+        fitKeys[splitline[0]] = splitline[1:] 
 
-    for index, name in enumerate(names) : 
-        fileName="%s_State/%s/hbond/rates.txt"%(state,name) 
+with open('fits/protein_chosen_decay.xvg') as f: 
+    lines = f.readlines() 
+    protFitKeys= {} 
+    for line in lines : 
+        splitline = line.split() 
+        protFitKeys[splitline[0]] = splitline[1:] 
 
-        with open(fileName) as f : 
-            for line in f.readlines() :
-                if 'Forward' in line : 
-                    rate = line.split()[2] 
-        print rate
-        rate = float(rate)
+xmax = 0 
+for index,file in enumerate(datafiles) : 
+    try : 
+        data = np.genfromtxt(file,skip_header=24) 
+        data[:,0] = data[:,0] / 1000 * 4
+    except : 
+        continue 
+
+    if (np.max(data[:,0]) > xmax) : xmax = np.max(data[:,0]) 
+
     
-        plt.errorbar(rate,slopes[index],label=name.split('_', 1)[1],marker='o',xerr=stds[index])
+    for i in np.arange(len(data)) : 
+        if data[i,5] == 0 : 
+            print "i = %i, data = %i"%(i,data[i,5]) 
+            break 
+    print i 
+    x = np.geomspace(data[1,0], data[i,0], 100) 
+    print data[1,0]
+    print x[0] 
+
+    ax = axarr[index/figCols,index%figCols]
+    if len(fitKeys[file.split('/')[1]] ) == 4 : 
+        print "Single exponential"
+        a, A, r, adjR = fitKeys[file.split('/')[1]]
+        a, A, r, adjR = float(a), float(A), float(r), float(adjR) 
+        A = A * 1000 / 4
+        y = a*np.exp(-1*A * x) 
+    elif len(fitKeys[file.split('/')[1]] ) == 6 : 
+        print "Double exponential"
+        ax.text(0.005,1,"*",color='b',fontsize='large') 
+        a, A, b, B, r, adjR = fitKeys[file.split('/')[1]]
+        a, A, b, B, r, adjR = float(a), float(A), float(b), float(B), float(r), float(adjR) 
+        A = A * 1000 / 4
+        B = B * 1000 / 4
+        y = a*np.exp(-1*A * x) + b* np.exp(-1* B* x) 
+
+    ##ax.loglog(data[:,0],data[:,4],color='tab:orange',label='Nearby water') 
+    ax.loglog(data[:,0],data[:,5],color='b',linestyle='-', label='HBonding water') 
+    ax.loglog(x,y,color='k',linestyle='-.') 
+#    ax.semilogy(data[:,0],data[:,5],color='b',linestyle='-', label='HBonding water') 
+#    ax.semilogy(x,y,color='k',linestyle='-.') 
+    ax.text(.2,  600, "r = %.3f"%(r) , fontsize=10,color='b') 
+
+    for i in np.arange(len(data)) : 
+        if data[i,6] == 0 : 
+            break 
+    x = np.geomspace(data[1,0], data[i,0], 100) 
+
+    if len(protFitKeys[file.split('/')[1]] ) == 4 : 
+        print "Single exponential"
+        a, A, r, adjR = protFitKeys[file.split('/')[1]]
+        a, A, r, adjR = float(a), float(A), float(r), float(adjR) 
+        A = A * 1000 / 4
+        y = a*np.exp(-1*A * x) 
+    elif len(protFitKeys[file.split('/')[1]] ) == 6 : 
+        print "Double exponential"
+        ax.text(0.01,1,"*",color='g',fontsize='large') 
+        a, A, b, B, r, adjR = protFitKeys[file.split('/')[1]]
+        a, A, b, B, r, adjR = float(a), float(A), float(b), float(B), float(r), float(adjR) 
+        A = A * 1000 / 4
+        B = B * 1000 / 4
+        y = a*np.exp(-1*A * x) + b* np.exp(-1* B* x) 
+
+    print file.split('/')[1]
+    if not (file.split('/')[1] == "GFP_N212X" or file.split('/')[1] == "GFP_F114X") : 
+        ax.loglog(data[:,0],data[:,6],color='g',label='Hbonding protein') 
+        ax.loglog(x,y,color='k',linestyle='-.') 
+#        ax.semilogy(data[:,0],data[:,6],color='g',label='Hbonding protein') 
+#        ax.semilogy(x,y,color='k',linestyle='-.') 
+        ax.text(.2,  200, "r = %.3f"%(r) , fontsize=10,color='g') 
+
+    ax.set_title(file.split('/')[1].split('_')[1]) 
+    #ax.set_ylim([0.8,2000]) 
+
+for index,file in enumerate(datafiles) : 
+    ax = axarr[index/figCols,index%figCols]
+    ax.set_xlim(0.004  ,5) 
+    ax.set_ylim(0.4,2000) 
     
-    index = 0
-
-    #for name in names : 
-#   #     if (name == 'GFP_Y92X' or name == 'GFP_M218X' or name == 'GFP_D190X' ) : 
-    #    if (name == 'GFP_Y92X' ) : 
-    ##        sasas = np.delete(sasas,index, None)
-    #        slopes= np.delete(slopes,index, None)
-    #        print "%s deleted"%name 
-    #        continue
-    #    index+=1
-    #
-    #slope,intercept,r_value,p_value,std_error = linregress(sasas,slopes) 
-    #print "slope = ",slope,"\tr_value = ",r_value 
-    
-    
-    #x = np.arange(0,3.0,0.01)
-    #plt.ylim([-0.065,-0.01]) 
-    #plt.plot(x,x*slope+intercept,label="r = %0.3f"%r_value)
-
-    box = ax.get_position() 
-    ax.set_position([box.x0, box.y0, box.width *0.75, box.height]) 
-
-    plt.legend(bbox_to_anchor=(1.5, 0.95),numpoints=1,fontsize='x-small')
-    plt.xlabel(r"Lifetime (ps)") 
-    plt.semilogx() 
-    plt.ylabel(r"$\frac{\partial \tilde{\nu}}{\partial T}$ (cm$^{-1}$ / K)") 
-
-
-    plt.savefig("%s/Lifetime_vs_slope_%s.pdf"%(saveDir,state),format='pdf') 
-    plt.close() 
+fig.savefig('figures/Lifetime_hbonds.pdf',format='pdf') 
+plt.close() 
 
