a36b19ca5d95d0c0eaae9b07fe6c415de68f238c
jeremyfirst22 <jeremy_first@utexas.edu>
2018-06-04 09:37:41 -0500

    Plot SASA of aromatic sidechain and nitrile as well as full residue, per reviewer request. Color codes added for comparison to other plots

--- Process_scripts/Process_SASA_vs_Slope.py
+++ Process_scripts/Process_SASA_vs_Slope.py
@@ -19,10 +19,10 @@ if not os.path.isdir(saveDir) :
     os.mkdir(saveDir)
 
 rc_file(rcFile) 
-mpl.rcParams['figure.figsize'] = 4.5,3
+mpl.rcParams['figure.figsize'] = 5.3,3
 
 fig, ax = plt.subplots(1,1) 
-fig.subplots_adjust(left=0.15,right=0.90,bottom=0.15) 
+fig.subplots_adjust(left=0.15,right=0.85,top=0.95,bottom=0.13) 
 
 names = np.genfromtxt(inFile,skip_header=1,usecols=0,dtype='str')
 slopes= np.genfromtxt(inFile,skip_header=1,usecols=1) 
@@ -42,9 +42,17 @@ nameToColorKeys = {
         }
 
 for index, name in enumerate(names) : 
-    fileName="B_State/%s/sasa/cnf_area.xvg"%(name) 
+    fileName="B_State/%s/sasa/nh_ct_area.xvg"%(name) 
     if os.path.isfile(fileName) : 
-        data = np.genfromtxt(fileName,skip_header=23)
+        countHLines = 0 
+        with open(fileName) as f : 
+            lines = f.readlines() 
+            for line in lines : 
+                if line.startswith('#') or line.startswith('@') : 
+                    countHLines += 1
+                else : 
+                    break 
+        data = np.genfromtxt(fileName,skip_header=countHLines)
         ##Step size is 4 ps. 2500 frames is 10 ns. Discarded as equiibration time. 
         sasas[index] = np.average(data[equilTime:,2]) 
         stds[index] = np.std(data[equilTime:,2]) 
@@ -71,11 +79,11 @@ print "slope = ",slope,"\tr_value = ",r_value
 first_legend = plt.legend(bbox_to_anchor=(1.34, 0.95),numpoints=1,edgecolor='k')
 plt.gca().add_artist(first_legend)
 
-x = np.arange(0,3.0,0.01)
+x = np.arange(0,np.max(sasas),0.01)
 plt.ylim([-0.065,-0.01]) 
 bestfit, = plt.plot(x,x*slope+intercept,label="r = %0.3f"%r_value,color='k',zorder=1)
 
-fig.text(0.64,0.82,r"r = %0.3f"%r_value) 
+fig.text(0.62,0.88,r"r = %0.3f"%r_value) 
 
 box = ax.get_position() 
 ax.set_position([box.x0, box.y0, box.width *0.85, box.height]) 
@@ -85,6 +93,6 @@ plt.xlabel(r"SASA (nm$^2$)")
 plt.ylabel(r"FTLS (cm$^{-1}$ / K)") 
 
 
-plt.savefig("%s/sasa_vs_slope.pdf"%(saveDir),format='pdf') 
+plt.savefig("%s/sasa_vs_slope_nitrile.pdf"%(saveDir),format='pdf') 
 plt.close() 
 
--- Process_scripts/Process_time_vs_sasa.py
+++ Process_scripts/Process_time_vs_sasa.py
@@ -13,6 +13,18 @@ figRows=3
 rcFile = 'rc_files/paper.rc'
 rc_file(rcFile) 
 
+nameToColorKeys = {
+        "F145X":'#A93226',
+        "M218X":'r',
+        "F165X":'#E67E22',
+        "F114X":'y',
+        "N198X":'#2ECC71',
+        "Y143X":'g',
+        "N212X":'c',
+        "D117X":'b',
+        "Y92X":'m'
+        }
+
 molecList = [
         "Y92X",
         "F114X",
@@ -44,55 +56,11 @@ for mol in molecList :
         data1[:,0] = data1[:,0] / 1000 
         equilTime = len(data1) / 5 
         data1 = data1[equilTime:] ##Discard first 10ns as equilibration time
-        ax.scatter(data1[:,0],data1[:,2],color='b',s=0.1) 
+        ax.scatter(data1[:,0],data1[:,2],color=nameToColorKeys[mol],s=0.1) 
     except IOError : 
         print "No file found for %s"%(datafile)  
-    except ValueError :
-        print "Trying %s again without bottom line."%datafile
-        data1 = np.genfromtxt(datafile,skip_header=27,skip_footer=1) 
-        data1[:,0] = data1[:,0] / 1000 
-        ax.scatter(data1[:,0],data1[:,2],color='b',s=0.1,label="Whole residue") 
-
-    #datafile = mol+'/sidechain.xvg'
-    #print datafile
-    #try : 
-    #    data1 = np.genfromtxt(datafile,skip_header=27) 
-    #    data1[:,0] = data1[:,0] / 1000 
-    #    ax.scatter(data1[:,0],data1[:,2],color='r',s=0.1,label="Sidechain only") 
-    #except IOError : 
-    #    print "No file found for %s"%(datafile)  
-    #except ValueError :
-    #    print "Trying %s again without bottom line."%datafile
-    #    data1 = np.genfromtxt(datafile,skip_header=27,skip_footer=1) 
-    #    data1[:,0] = data1[:,0] / 1000 
-    #    ax.scatter(data1[:,0],data1[:,2],color='r',s=0.1) 
-
-    #datafile = mol+'/nit_4_atoms_area.xvg'
-    #try : 
-    #    data2 = np.genfromtxt(datafile,skip_header=27) 
-    #    data2[:,0] = data2[:,0] / 1000 
-    #    ax.scatter(data2[:,0],data2[:,2],color='g',s=0.1,label="5 atom area") 
-    #except IOError : 
-    #    print "No file found for %s"%(datafile)  
-    #except ValueError :
-    #    print "Trying %s again without bottom line."%datafile
-    #    data2 = np.genfromtxt(datafile,skip_header=27,skip_footer=1) 
-    #    data2[:,0] = data2[:,0] / 1000 
-    #    ax.scatter(data2[:,0],data2[:,2],color='g',s=0.1) 
-
-    #datafile = mol+'/nh_ct_area.xvg'
-    #try : 
-    #    data3 = np.genfromtxt(datafile,skip_header=27) 
-    #    data3[:,0] = data3[:,0] / 1000 
-    #    ax.scatter(data3[:,0],data3[:,2],color='b',s=0.1,label="C N atoms only" ) 
-    #except IOError : 
-    #    print "No file found for %s"%(datafile)  
-    #except ValueError :
-    #    print "Trying %s again without bottom line."%datafile
-    #    data3 = np.genfromtxt(datafile,skip_header=27,skip_footer=1) 
-    #    data3[:,0] = data3[:,0] / 1000 
-    #    ax.scatter(data3[:,0],data3[:,2],color='b',s=0.1,label="C N atoms only") 
-
+    except : 
+        print "Error importing data from file %s"%(datafile)
 
     ax.set_title(datafile.split('/')[1].split('_')[1]) 
     ax.set_xlim(10,50) 
@@ -101,6 +69,70 @@ for mol in molecList :
     index +=1
 
 fig.savefig('figures/sasa_v_time.png',format='png') 
+plt.close() 
+
+fig, axarr = plt.subplots(figRows,figCols,sharex='col',sharey='row') 
+fig.subplots_adjust(wspace=0.1,hspace=0.35,left=0.15) 
+fig.text(0.5,0.04, r"Time (ns)", ha='center', va='center') 
+fig.text(0.03,0.5, r"SASA (nm$^2$)", ha='center', va='center',rotation='vertical') 
+
+index=0
+for mol in molecList : 
+    print index, index/figCols, index%figRows
+    ax = axarr[index/figCols,index%figCols]
+
+    datafile = 'B_State/GFP_%s/sasa/aromatic.xvg'%mol
+    print datafile
+    try : 
+        data1 = np.genfromtxt(datafile,skip_header=27) 
+        data1[:,0] = data1[:,0] / 1000 
+        equilTime = len(data1) / 5 
+        data1 = data1[equilTime:] ##Discard first 10ns as equilibration time
+        ax.scatter(data1[:,0],data1[:,2],color=nameToColorKeys[mol],s=0.1) 
+    except IOError : 
+        print "No file found for %s"%(datafile)  
+    except : 
+        print "Error importing data from file %s"%(datafile)
+
+    ax.set_title(datafile.split('/')[1].split('_')[1]) 
+    ax.set_xlim(10,50) 
+    ax.set_ylim(0,2.5)     
+
+    index +=1
+
+fig.savefig('figures/sasa_v_time_aromatic.png',format='png') 
+plt.close() 
+
+fig, axarr = plt.subplots(figRows,figCols,sharex='col',sharey='row') 
+fig.subplots_adjust(wspace=0.1,hspace=0.35,left=0.15) 
+fig.text(0.5,0.04, r"Time (ns)", ha='center', va='center') 
+fig.text(0.03,0.5, r"SASA (nm$^2$)", ha='center', va='center',rotation='vertical') 
+
+index=0
+for mol in molecList : 
+    print index, index/figCols, index%figRows
+    ax = axarr[index/figCols,index%figCols]
+
+    datafile = 'B_State/GFP_%s/sasa/nh_ct_area.xvg'%mol
+    print datafile
+    try : 
+        data1 = np.genfromtxt(datafile,skip_header=27) 
+        data1[:,0] = data1[:,0] / 1000 
+        equilTime = len(data1) / 5 
+        data1 = data1[equilTime:] ##Discard first 10ns as equilibration time
+        ax.scatter(data1[:,0],data1[:,2],color=nameToColorKeys[mol],s=0.1) 
+    except IOError : 
+        print "No file found for %s"%(datafile)  
+    except : 
+        print "Error importing data from file %s"%(datafile)
+
+    ax.set_title(datafile.split('/')[1].split('_')[1]) 
+    ax.set_xlim(10,50) 
+    ax.set_ylim(0,0.75)    
+
+    index +=1
+
+fig.savefig('figures/sasa_v_time_nitrile.png',format='png') 
 plt.close() 
 
 fig, axarr = plt.subplots(figRows,figCols,sharex='col',sharey='row') 
